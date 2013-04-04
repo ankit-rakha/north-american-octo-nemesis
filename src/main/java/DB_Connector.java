6fb50de708b37b97aba34aea90bc49015502cf89
Ankit Rakha <ankit.rakha@gmail.com>
2013-04-04 00:08:29 -0700

    added multithreading capability

--- src/main/java/DB_Connector.java
+++ src/main/java/DB_Connector.java
@@ -3,22 +3,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.String;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.StringTokenizer;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DB_Connector
 {
-    private static String DB_PATH = "", INPUT_FILE = "", resultString, sCurrentLine, node_first_id, node_second_id, nodes_relationship;
-    private long time, total, initialTime, compTime,node_init,node_total,rel_init,rel_total;
+    private static String DB_PATH = "", INPUT_FILE = "", sCurrentLine, node_first_id, node_second_id, nodes_relationship;
+    private long time, total, initialTime, compTime, node_init, node_total, rel_init, rel_total;
     private long node_id1, node_id2;
     private Map<String, Object> params = new HashMap<String, Object>();
     private final Map<String, String> config = new HashMap<String, String>();
+    private GraphDatabaseService db;
+    private ExecutionEngine engine;
+    private static final int NTHREDS = 5;
 
     public static void main( String[] args )
     {
@@ -26,7 +28,35 @@ public class DB_Connector
         DB_Connector.INPUT_FILE = args[1].trim();
         DB_Connector DB_Connector = new DB_Connector();
         DB_Connector.configParameters();
-        DB_Connector.createDatabasae();
+        DB_Connector.createDatabase();
+        DB_Connector.generateTriplet(DB_Connector.db, DB_Connector.engine);
+
+        ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new MyRunnable(i,DB_Connector.db, DB_Connector.engine, 1, 2, "is_a");
+            executor.execute(worker);
+        }
+        // This will make the executor accept no new threads
+        // and finish all existing threads in the queue
+        executor.shutdown();
+        // Wait until all threads are finish
+        while (!executor.isTerminated()) {
+
+        }
+        System.out.println("Finished all threads");
+
+        /*MyRunnable myRunnable1 = new MyRunnable(DB_Connector.db, DB_Connector.engine, 1, 2, "is_a");
+        Thread t1 = new Thread(myRunnable1);
+        t1.start();
+        MyRunnable myRunnable2 = new MyRunnable(DB_Connector.db, DB_Connector.engine,3, 4, "is_a");
+        Thread t2 = new Thread(myRunnable2);
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
     }
 
     public void configParameters(){
@@ -37,14 +67,14 @@ public class DB_Connector
         config.put( "neostore.propertystore.db.arrays.mapped_memory", "1G" );
     }
 
-    public void createDatabasae(){
+    public void createDatabase(){
 
-        GraphDatabaseService db = new GraphDatabaseFactory()
+        db = new GraphDatabaseFactory()
                 .newEmbeddedDatabaseBuilder( DB_PATH )
                 .setConfig( config )
                 .newGraphDatabase();
 
-        ExecutionEngine engine = new ExecutionEngine( db );
+        engine = new ExecutionEngine( db );
 
         System.out.println("Processing nodes ..");
         node_init = System.currentTimeMillis();
@@ -58,10 +88,9 @@ public class DB_Connector
         rel_total = (System.currentTimeMillis() - rel_init);
         System.out.println("took " + rel_total + "ms");
 
-        checkTriplet(db, engine);
     }
 
-    public synchronized void checkTriplet(GraphDatabaseService db, ExecutionEngine engine)
+    public void generateTriplet(GraphDatabaseService db, ExecutionEngine engine)
     {
         BufferedReader br = null;
 
@@ -87,25 +116,7 @@ public class DB_Connector
 
                     nodes_relationship = st1.nextElement().toString();
 
-                    params.put( "id1", node_id1 );
-                    params.put( "id2", node_id2 );
-
-                    time = System.currentTimeMillis();
-
-                    ExecutionResult result = engine.execute( "START node1=node({id1}),node2=node({id2}) MATCH (node1)-[:"+nodes_relationship+"]->(node2) RETURN node1", params );
-                    total = (System.currentTimeMillis() - time);
-
-                    Iterator<Node> checkNode = result.columnAs("node1");
-
-                    if (checkNode.hasNext()) {
-
-                        System.out.println("Found");
-                        System.out.println("took " + total + "ms");
-                    }
-                    else {
-                        System.out.println("Not Found");
-                        System.out.println("took " + total + "ms");
-                    }
+                    runQuery(engine,node_id1,node_id2,nodes_relationship);
 
                 }
 
@@ -131,6 +142,27 @@ public class DB_Connector
 
         compTime = (System.currentTimeMillis() - initialTime);
         System.out.println("-- TOTAL TIME: " + compTime + "ms");
+    }
+
+    void runQuery(ExecutionEngine engine,long node_id1,long node_id2,String nodes_relationship){
+
+        params.put( "id1", node_id1 );
+        params.put( "id2", node_id2 );
+
+        time = System.currentTimeMillis();
+
+        if (engine.execute( "START node1=node({id1}),node2=node({id2}) MATCH (node1)-[:"+nodes_relationship+"]->(node2) RETURN node1", params).columnAs("node1").hasNext()){
+            total = (System.currentTimeMillis() - time);
+            System.out.println("Found");
+            System.out.println("took " + total + "ms");
+        }
+        else {
+            total = (System.currentTimeMillis() - time);
+            System.out.println("Not Found");
+            System.out.println("took " + total + "ms");
+
+        }
+
     }
 
 }
---
+++ src/main/java/MyRunnable.java
@@ -0,0 +1,60 @@
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Map;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.GraphDatabaseService;
+
+public class MyRunnable implements Runnable {
+
+    private static String nodes_relationship;
+    private long time, total;
+    private long node_id1, node_id2;
+    private Map<String, Object> params = new HashMap<String, Object>();
+    private GraphDatabaseService db;
+    private ExecutionEngine engine;
+    private int i;
+
+    public MyRunnable( int i, GraphDatabaseService db, ExecutionEngine engine, long node_id1, long node_id2, String nodes_relationship ) {
+        this.i = i;
+        this.node_id1 = node_id1;
+        this.node_id2 = node_id2;
+        this.nodes_relationship = nodes_relationship;
+        this.db = db;
+        this.engine = engine;
+    }
+
+    public void run() {
+        try {
+
+            params.put( "id1", node_id1 );
+            params.put( "id2", node_id2 );
+
+            time = System.currentTimeMillis();
+
+            engine.execute( "START node1=node({id1}),node2=node({id2}) MATCH (node1)-[:"+nodes_relationship+"]->(node2) RETURN node1", params);
+
+            System.out.println("took " + total + "ms .. processed by thread " + i);
+
+            /*if (engine.execute( "START node1=node({id1}),node2=node({id2}) MATCH (node1)-[:"+nodes_relationship+"]->(node2) RETURN node1", params).columnAs("node1").hasNext()){
+                total = (System.currentTimeMillis() - time);
+                System.out.println("Found");
+                System.out.println("took " + total + "ms");
+            }
+            else {
+                total = (System.currentTimeMillis() - time);
+                System.out.println("Not Found");
+                System.out.println("took " + total + "ms");
+
+            }*/
+
+            Thread.sleep(500);
+
+
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted.");
+        }
+
+        db.shutdown();
+
+    }
+}
